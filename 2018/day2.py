1c97c1ecd9164334c5c968ef9735713bd570f09f
Kailash Sankar <Kailash.Sankar@outlook.com>
2019-12-03 14:43:17 +0530

    added part two

--- 2018/day2.py
+++ 2018/day2.py
@@ -1,7 +1,6 @@
 
-
 def get_checksum():
-    fin = open('input_day2.txt','r')
+    fin = open('input.txt','r')
     count ={ 'two': 0, 'three': 0 }
     
     for line in fin:
@@ -18,10 +17,38 @@ def get_checksum():
             count['two']+=1
         if 3 in vlist:
             count['three']+=1
-        
+    
+    fin.close()
     return count['two'] * count['three']
 
+def find_seq():
+    fin = open('input.txt','r')
+    inputs = []
+    for line in fin:
+        inputs.append(line.strip('\n'))
+    noi = len(inputs)
+
+    # brute force comparison
+    for i in range(0,noi): 
+        for j in range(i+1,noi):
+            x = inputs[i]
+            y = inputs[j]
+            counter = 0
+            for a,b in zip(x,y):
+                if a != b:
+                    counter+=1
+                    if counter > 1:
+                        break
+            if counter <= 1:
+                print('matches', x , y)
+                z = ""
+                for a,b in zip(x,y):
+                    if a == b:
+                        z+=a
+                return z
 
 if __name__ == '__main__':
     checksum = get_checksum()
-    print('count', checksum);
+    print('count', checksum)
+    seq = find_seq()
+    print('sequence', seq)
